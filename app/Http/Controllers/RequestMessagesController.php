08a4dc05f0cf3b84251cd34e49d21e014525ad96
tunji oyeniran <oyenirantunji2339@gmail.com>
2019-12-10 09:06:43 +0100

    created RequestMessages model with migration controller and factory

---
+++ app/Http/Controllers/RequestMessagesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\RequestMessages;
+use Illuminate\Http\Request;
+
+class RequestMessagesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\RequestMessages  $requestMessages
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RequestMessages $requestMessages)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\RequestMessages  $requestMessages
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(RequestMessages $requestMessages)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\RequestMessages  $requestMessages
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, RequestMessages $requestMessages)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\RequestMessages  $requestMessages
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RequestMessages $requestMessages)
+    {
+        //
+    }
+}
